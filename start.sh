e1065e8025f4d8c18273ced33d863e7d8bf5df14
frogconn <frogconn@gmail.com>
2021-10-16 20:28:14 +0700

    Implement deploy with node adapter.

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
+## SPA Mode
 docker build . -t svelte-docker
 
 docker run --rm --name=svelte-docker -p 5000:80 svelte-docker
+
+## SSR Mode
+
+docker build . -t svelte-docker -f Dockerfile.ssr
+
+docker run --rm --name=svelte-docker -p 5000:3000 svelte-docker
